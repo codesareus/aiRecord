e3990bc8c5332726b811be05c362add954db9ab8
codesareus <125232658+codesareus@users.noreply.github.com>
2025-02-20 09:44:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -252,14 +252,7 @@ def main():
         if st.session_state.expand_all:
             # Show all paragraphs as a single block for easy copying (preserving highlights)
             full_text = "<br><br>".join(st.session_state.matching_paragraphs)
-            # ###Create a text area for user input
-            edited_text = st.text_area("Edit the text:", full_text, height=200)
-            # ###-Render the edited text with markdown
-            # ######Display the rendered HTML
-            if st.button("Editable"):
-                st.markdown("### Preview:")
-            st.markdown(edited_text, unsafe_allow_html=True)
-            #st.markdown(full_text, unsafe_allow_html=True)
+            st.markdown(full_text, unsafe_allow_html=True)
 
             # Copy button (removes HTML tags before copying)
             if st.button("Copy"):

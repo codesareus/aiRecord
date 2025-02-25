15b7d07fbf87555d4127dae7607a2efa9b9c3851
codesareus <125232658+codesareus@users.noreply.github.com>
2025-02-25 08:53:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -309,9 +309,13 @@ def main():
                 dbytd = datetime.now(midwest) - timedelta(days=2)
                 st.session_state.matching_paragraphs = get_paragraphs_by_date(st.session_state.file_content, dbytd)
                 st.rerun()
+            elif st.button("-3days"):
+                dbytd = datetime.now(midwest) - timedelta(days=3)
+                st.session_state.matching_paragraphs = get_paragraphs_by_date(st.session_state.file_content, dbytd)
+                st.rerun()
             else:
                 st.warning("No file content available.")
-                
+                       
     with col2:
         if st.button("ytDay"):
             if st.session_state.get("file_content"):

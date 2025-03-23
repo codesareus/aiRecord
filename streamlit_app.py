1a60d008a56a7aed63db2fde8535a6aaa2da2da4
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-23 04:46:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,7 +196,8 @@ def main():
 
     recentR= st.checkbox("show recent records")
     if recentR:
-        st.code(f"Recent: {st.session_state.file_content[-4000:]}")
+        st.session_state.text_area_content = f"Recent: {st.session_state.file_content[-4000:]}"
+        #st.code(f"Recent: {st.session_state.file_content[-4000:]}")
     else:
         content_without_whitespace = "".join(st.session_state.file_content[-20:-1].split())# space is cause line breaks in display
         st.code(f"Last: {content_without_whitespace}...{st.session_state.file_content.split("\n\n")[0]}")

349b9d0c7a32688e0e82e7db0f237206b1ab9b1f
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-24 19:04:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -275,31 +275,26 @@ def main():
 
     # Secret key input
     secret_key = st.text_input("Enter the secret key to enable saving:", type="password")
-    save_button_disabled = secret_key != "zzzzzzzzz" or user_text == ""
+    save_button_disabled = secret_key != "zzzzzzzzz" or user_text == "" or st.session_state.text_area_content == ""
 
     # Save text button
     col1, col2=st.columns(2)
-    saveok = True
+    
     with col1:
         if st.button("Save Text", disabled=save_button_disabled):
-            if  user_text != "" and user_text.strip() and saveok :
+            if  user_text != "" and user_text.strip() :
                 save_text_to_file(user_text)
-            #st.session_state.text_area_content = ""
+                st.session_state.text_area_content = ""
             #user_text =""
                 st.session_state.show_confirmation = True
-                saveok = False
                 st.rerun()  
             else:
                 st.write("maybe saved already")
 
     with col2:
-        if st.button("clear input" if not saveok else "activate save" ):
-            if not saveok:
-                st.session_state.text_area_content=""
-                saveok = True
-                st.rerun()  
-            
-                
+        if st.button("activate save"):
+            st.session_state.text_area_content = user_text
+            st.rerun()
     # Show confirmation message and ClearInput button
     if st.session_state.get("show_confirmation", False):
         st.success("Text saved successfully!")
